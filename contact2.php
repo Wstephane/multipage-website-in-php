9e67c91980c6054173b437663100cd247ab76ad2
Wyns Stéphane <stewyns.pro@gmail.com>
2018-08-01 12:13:18 +0200

    Version 0.1 Formulaire

---
+++ contact2.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Hakan Poyraz et Stéphane Wyns">
+
+    <title>Télé-Accueil Bruxelles - Quelqu'un à qui parler dans l'anonymat</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <!-- <link href="css/business-casual.min.css" rel="stylesheet"> -->
+
+  </head>
+
+  <body>
+
+    <h1>Contact</h1>
+
+    <form class="form-horizontal" role="form">
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="inlineCheckbox1" value="option1">
+            <label class="form-check-label" for="inlineCheckbox1">Mme</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="inlineCheckbox2" value="option2">
+            <label class="form-check-label" for="inlineCheckbox2">Melle</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="inlineCheckbox3" value="option3">
+            <label class="form-check-label" for="inlineCheckbox3">Mr.</label>
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput">Nom</label>
+            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Nom">
+        </div>
+        <div class="form-group">
+            <label for="formGroupExampleInput2">Prénom</label>
+            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Prénom">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email">
+        </div>
+        <div class="form-row align-items-center">
+            <div class="col-auto my-1">
+                <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
+                    <option selected>Demande d'information</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Votre message</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="exampleFormControlFile1">Document</label>
+            <input type="file" class="form-control-file" id="exampleFormControlFile1">
+        </div>
+        <div class="form-check form-check-inline">
+            <p>Format de réponse souhaité</p>
+            <input class="form-check-input" type="radio" id="inlineCheckbox1" value="option1">
+            <label class="form-check-label" for="inlineCheckbox1">HTML</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" id="inlineCheckbox2" value="option2">
+            <label class="form-check-label" for="inlineCheckbox2">Texte</label>
+        </div>
+        <button type="submit" class="btn btn-default">Contactez-moi</button>
+
+    </form>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
