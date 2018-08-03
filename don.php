ed810e0d7d7bd648700ae41cb9f3a71257a03f2d
Wyns Stéphane <stewyns.pro@gmail.com>
2018-08-03 17:30:35 +0200

    Version HTML presque terminer

---
+++ don.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Hakan Poyraz et Stéphane Wyns">
+
+    <title>Télé-Accueil Bruxelles - Quelqu'un à qui parler dans l'anonymat</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/business-casual.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <h1 class="site-heading text-center text-white d-none d-lg-block">
+      <span class="site-heading-upper text-primary mb-3">Quelqu'un à qui parler dans l'anonymat</span>
+      <span class="site-heading-lower">Télé-Accueil Bruxelles</span>
+    </h1>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item active px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="index.php">Accueil
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="benevole.php">Devenir écoutant Bénèvole</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="formation.php">Formation</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="don.php">Don</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="contact.php">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <section class="page-section clearfix">
+      <div class="container">
+        <div class="intro">
+          <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="img/intro.jpg" alt="">
+          <div class="intro-text left-0 text-center bg-faded p-5 rounded">
+            <h2 class="section-heading mb-4">
+              <span class="section-heading-upper">Depuis 1959</span>
+              <span class="section-heading-lower">Notre Projet</span>
+            </h2>
+            <p class="mb-3">Télé-Accueil Bruxelles propose une écoute à toute personne en difficulté sur le plan moral, social ou psychologique et qui souhaite en parler dans l’anonymat et la confidentialité. L’écoute est assurée par des bénévoles <strong>24h/24 et 7j/7</strong> grâce au <strong>numéro de téléphone gratuit 107 et par internet tous les jours de 13h à 03 heures</strong> sur <a href="https://www.sos-amitie.com/web/internet/chat">le site www.chat-accueil.org</a>. Ces permanences Chat sont assurées en partenariat avec <a href="https://www.sos-amitie.org/">SOS Amitié-France</a>.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+    <section class="page-section cta">
+      <div class="container">
+        <!-- <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/about.jpg" alt=""> -->
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <span class="section-heading-upper">Le CEFEC et</span>
+                  <span class="section-heading-lower">L'observatoire Social</span>
+                </h2>
+                <p>Deux autres activités existent également au sein de l’association : le CEFEC et l’Observatoire social.</p>
+                <ul>
+                  <li><a href="formation.php">Le CEFEC</a>, Centre de formation à l’écoute, a été créé pour promouvoir l’écoute et l’accueil. Il organise des formations et des supervisions destinées aux bénévoles et aux professionnels extérieurs à Télé-Accueil Bruxelles.</li>
+                  <li><a href="formation.php#here">L’Observatoire social</a> est chargé de refléter auprès du public et des politiques les phénomènes sociaux et les évolutions que Télé-Accueil Bruxelles constate depuis sa position privilégiée d’écoutant, et ce tout en préservant l’anonymat des appelants et la confidentialité de leurs propos. Régulièrement, il publie des études sur des thématiques particulières.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="page-section cta">
+      <div class="container">
+        <!-- <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/about.jpg" alt=""> -->
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <!-- <span class="section-heading-upper"></span> -->
+                  <span class="section-heading-lower">Historique</span>
+                </h2>
+                <p>Télé-Accueil Bruxelles existe depuis plus de cinquante ans !</p>
+                <ul>
+                  <li><strong>1953 :</strong> L’idée de proposer une écoute par téléphone vient de Grande-Bretagne. Suite au suicide d’un adolescent de 14 ans, le révérend Chad Varah publia une annonce dans le Times du 2 novembre 1953 : « Avant de vous suicider, appelez-moi à M.A.N. 90.00 ». Il inventa ainsi le premier service d’écoute téléphonique, un service pour venir en aide à tous ceux qui étaient tentés par le suicide, aux désespérés, aux isolés et à tous ceux qui ne se sentaient écoutés de personne. Rapidement, d'autres postes d’écoute vont s'ouvrir dans le monde.</li>
+                  <li><strong>1959 :</strong> création du premier poste de Télé-Accueil à Bruxelles.</li>
+                  <li><strong>1967 :</strong> création d’une fédération internationale IFOTES (Fédération internationale des Services de Secours par téléphone).</p>
+                  <li><strong>1973 :</strong> Télé-Accueil devient une asbl.</li>
+                  <li><strong>1976 :</strong> Télé-Accueil est reconnu comme service d’utilité publique.</li>
+                  <li><strong>1977 :</strong> création de la Fédération belge des Télé-Accueil francophones (6 postes).</li>
+                  <li><strong>1990 :</strong> création du CEFEC (Centre de Formation à l’écoute).</li>
+                  <li><strong>1992 :</strong> attribution d’un numéro d’urgence gratuit : le 107.</li>
+                  <li><strong>1998 :</strong> création de l’Observatoire social.</li>
+                  <li><strong>2005 :</strong> Télé-Accueil diversifie son offre d’écoute et devient la première association en Belgique francophone à offrir une écoute par « chat » sur internet : le Chat-Accueil.</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="footer text-faded text-center py-5">
+      <div class="container">
+        <p class="m-0 small">Copyright &copy; Your Website 2018</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+</html>
