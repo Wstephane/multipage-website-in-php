bb9946b13b6a2689c9fcd56e51dc6b404ccbfebe
Wyns Stéphane <stewyns.pro@gmail.com>
2018-08-03 15:46:40 +0200

    Version HTML terminer

---
+++ benevole.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Hakan Poyraz et Stéphane Wyns">
+
+    <title>Télé-Accueil Bruxelles - Quelqu'un à qui parler dans l'anonymat</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/business-casual.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <h1 class="site-heading text-center text-white d-none d-lg-block">
+      <span class="site-heading-upper text-primary mb-3">Quelqu'un à qui parler dans l'anonymat</span>
+      <span class="site-heading-lower">Télé-Accueil Bruxelles</span>
+    </h1>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item active px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="index.php">Accueil
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="benevole.php">Devenir écoutant Bénèvole</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="formation.php">Formation</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="don.php">Don</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="contact.php">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <section class="page-section about-heading">
+      <div class="container">
+        <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/about.jpg" alt="">
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <!-- <span class="section-heading-upper"></span> -->
+                  <span class="section-heading-lower">Comment devenir écoutant</span>
+                </h2>
+                <p>Vous souhaitez vous engager dans une activité solidaire enrichissante...
+                  Vous êtes convaincu de l’importance de la parole et de l’écoute...
+                  Vous aimez aller à la rencontre d'autres personnes...
+                  Vous êtes prêt à suivre une formation à l’écoute...
+                  Vous disposez de quelques heures par semaine… 
+                  <strong>Devenez écoutant bénévole à Télé-Accueil Bruxelles !</strong></p>
+                <!-- <p class="mb-0"></p> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="page-section cta">
+      <div class="container">
+        <!-- <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/about.jpg" alt=""> -->
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <!-- <span class="section-heading-upper"></span> -->
+                  <span class="section-heading-lower">Une formation continuel</span>
+                </h2>
+                <ul>
+                  <li><strong>Une formation initiale</strong>. Accueillir et écouter à Télé-Accueil ne s’improvisant pas, nous assurons une formation initiale à l’écoute. Celle-ci permet de découvrir le cadre et les attitudes d’écoute soutenues par l’institution.</li>
+                  <li><strong>Une formation continue</strong>. Une fois par mois, chaque écoutant participe à une supervision de deux heures en compagnie d’autres écoutants et d'un professionnel. Ces supervisions permettent à chacun de partager ses questions et difficultés surgies lors d’un appel. La formation continue comprend également la participation gratuite à des ateliers, des conférences ainsi qu’à notre matinée d’étude annuelle.</li>
+                  <li><strong>Un encadrement professionnel continu</strong>. L’écoutant reçoit un soutien ponctuel face aux appels qui posent question et un accompagnement quant à son engagement.</li>
+                </ul>
+                <p class="mb-0"></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="page-section cta">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-9 mx-auto">
+            <div class="cta-inner text-center rounded">
+              <h1>Questions les plus fréquentes.</h1>
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-upper">Peut-on écouter de chez soi ?</span>
+              </h2>
+              <p class="mb-0">Non. Accueillir et écouter quelqu’un demande une certaine disponibilité de même que certaines conditions. Afin que celles-ci soient remplies au mieux, l’écoute se fait dans des bureaux destinés à cet effet. Ils sont facilement accessibles en transports en commun ou en voiture.</p>
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-upper">Faut-il un diplôme ou une formation particulière pour devenir écoutant ?</span>
+              </h2>
+              <p class="mb-0">Non. Toute personne intéressée par la rencontre avec l’autre et prête à questionner ses attitudes d’accueil, d’écoute et sa motivation à devenir écoutant peut devenir candidate.</p>
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-upper">Est-ce possible d'être écoutant quand on travaille à temps plein ? </span>
+              </h2>
+              <p class="mb-0">Oui. Il est possible de faire des permanences d'écoute en soirée ou le week-end. Par ailleurs, toutes les formations sont programmées en soirée ou le week-end.</p>
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-upper">Y a-t-il des facilités pour les permanences d'écoute durant la nuit ? </span>
+              </h2>
+              <p class="mb-0">Oui. Il est possible de loger sur place avant ou après une permanence. Les trajets de nuit en taxi peuvent être remboursés.</p>
+              <h2 class="section-heading mb-4">
+                <span class="section-heading-upper">Les frais de déplacement sont-ils pris en compte ?</span>
+              </h2>
+              <p class="mb-0">Oui, sous certaines conditions.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="footer text-faded text-center py-5">
+      <div class="container">
+        <p class="m-0 small">Copyright &copy; Your Website 2018</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
---
+++ formation.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Hakan Poyraz et Stéphane Wyns">
+
+    <title>Télé-Accueil Bruxelles - Quelqu'un à qui parler dans l'anonymat</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/business-casual.min.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <h1 class="site-heading text-center text-white d-none d-lg-block">
+      <span class="site-heading-upper text-primary mb-3">Quelqu'un à qui parler dans l'anonymat</span>
+      <span class="site-heading-lower">Télé-Accueil Bruxelles</span>
+    </h1>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark py-lg-4" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand text-uppercase text-expanded font-weight-bold d-lg-none" href="#">Start Bootstrap</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav mx-auto">
+            <li class="nav-item active px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="index.php">Accueil
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="benevole.php">Devenir écoutant Bénèvole</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="formation.php">Formation</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="don.php">Don</a>
+            </li>
+            <li class="nav-item px-lg-4">
+              <a class="nav-link text-uppercase text-expanded" href="contact.php">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <section class="page-section about-heading">
+      <div class="container">
+        <!-- <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/about.jpg" alt=""> -->
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <span class="section-heading-upper">En quelques mots</span>
+                  <span class="section-heading-lower">Le CEFEC</span>
+                </h2>
+                <p>Dans le but de promouvoir, de favoriser et d'encourager l’écoute et l’accueil, Télé-Accueil Bruxelles a créé le CEFEC (Centre de Formation à l'Ecoute). Depuis 1992, il organise des formations ainsi que des supervisions destinées aux bénévoles et aux professionnels extérieurs à Télé-Accueil Bruxelles.</p>
+                <!-- <p class="mb-0"></p> -->
+                <ul>
+                  <li><strong>Formations « groupe pluri-institutionnel »</strong> pour professionnels ou bénévoles d'horizons divers concernés par l'écoute et l'accueil au téléphone et/ou en face à face.</li>
+                  <li><strong>Formations « groupe mono-institutionnel »</strong> pour toute équipe de professionnels ou de bénévoles du secteur éducatif, pédagogique, psychosocial, de la santé ou autre concernée par l'écoute et l'accueil au téléphone et/ou en face à face.</li>
+                  <li><strong>Supervisions</strong> pour toute équipe de professionnels ou de bénévoles du secteur éducatif, pédagogique, psychosocial, de la santé ou autre concernée par l'écoute et l'accueil au téléphone et/ou en face à face.</li>
+                  <li><strong>Groupes d’échanges de pratique</strong> pour toute équipe de professionnels ou de bénévoles du secteur éducatif, pédagogique, psychosocial, de la santé ou autre concernée par l'écoute et l'accueil au téléphone et/ou en face à face.</li>
+                </ul>
+                <p>Ces formations ne sont pas destinées aux personnes qui souhaitent devenir écoutant à Télé-Accueil Bruxelles. Si vous voulez faire partie de notre équipe d’écoutants, consultez la page <a href="benevole.php">« Devenir écoutant bénévole »</a>.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+        <section class="page-section cta">
+      <div class="container">
+        <!-- <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0" src="img/about.jpg" alt=""> -->
+        <div class="about-heading-content">
+          <div class="row">
+            <div class="col-xl-9 col-lg-10 mx-auto">
+              <div class="bg-faded rounded p-5">
+                <h2 class="section-heading mb-4">
+                  <!-- <span class="section-heading-upper"></span> -->
+                  <span class="section-heading-lower">Les Formateurs</span>
+                </h2>
+                <p>Les formateurs du CEFEC ont une pratique multiple : clinique, thérapeutique, sociale. Ils ont l'expérience de formations et de supervisions dans le champ de la promotion de la santé et de la santé mentale. Par ailleurs, ils partagent régulièrement des temps d’échanges de pratique (supervisions).</p>
+                <p><u>Les Formateurs :</u></p>
+                <ul>
+                  <li><strong>Myriam Machurot.</strong> Superviseuse, psychologue clinicienne et psychodramatiste.</li>
+                  <li><strong>Alain Dekeuleneer.</strong> Superviseur, psychologue clinicien et psychodramastiste.</li>
+                  <li><strong>Pascal Kayaert.</strong> Superviseur, directeur de Télé-Accueil Bruxelles, travailleur social. Formateur à l’asbl Repères (promotion de la santé).</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="footer text-faded text-center py-5">
+      <div class="container">
+        <p class="m-0 small">Copyright &copy; Your Website 2018</p>
+      </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
