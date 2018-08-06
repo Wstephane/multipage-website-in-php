5e5091745e01df3c8623402c4f922b04e687cd74
hakan poyraz <hakan.poyraz1994@gmail.com>
2018-08-06 14:52:48 +0200

    Footer Done

--- footer.php
+++ footer.php
@@ -1,3 +1,9 @@
 <?php
 
+<p>© 2005-2014 Télé-Accueil Bruxelles asbl - <a href="#">Intranet</a></p>
+<p>Agréé par la Commission communautaire française - <a href="#">chat</a></p></div>
+<p class="art-page-footer">
+      <span id="art-footnote-links">Designed by <a href="http://www.informaction.be" target="_blank">InformAction</a>.</span>
+  </p>
+</div>
  ?>
--- upload.php
+++ upload.php
@@ -1,13 +1,10 @@
 <?php
   $handle = new upload($_FILES['image_field']);
   if ($handle->uploaded) {
-    $handle->file_new_name_body   = 'image_resized';
-    $handle->image_resize         = true;
-    $handle->image_x              = 100;
-    $handle->image_ratio_y        = true;
+    $handle->file_name_body_add    = "_".round(microtime(true));
+    if ($handle->file_src_name_ext === 'jpg' || $handle->file_src_name_ext === 'jpeg' || $handle->file_src_name_ext === 'png' || $handle->file_src_name_ext === 'gif')
     $handle->process('/home/user/files/');
     if ($handle->processed) {
-      echo 'image resized';
       $handle->clean();
     } else {
       echo 'error : ' . $handle->error;
