5ab6c96ac6ce0941dbe7a9aa47503dee09d1f9b4
hakan poyraz <hakan.poyraz1994@gmail.com>
2018-08-06 16:33:39 +0200

    Formulaire presque avance

--- upload.php
+++ upload.php
@@ -1,13 +1,19 @@
 <?php
+require 'vendor/autoload.php';
   $handle = new upload($_FILES['image_field']);
   if ($handle->uploaded) {
+    // Ajout d'une fin pour que le fichier soi unique
     $handle->file_name_body_add    = "_".round(microtime(true));
-    if ($handle->file_src_name_ext === 'jpg' || $handle->file_src_name_ext === 'jpeg' || $handle->file_src_name_ext === 'png' || $handle->file_src_name_ext === 'gif')
-    $handle->process('/home/user/files/');
-    if ($handle->processed) {
-      $handle->clean();
-    } else {
-      echo 'error : ' . $handle->error;
-    }
-  }
+  // Vérification du fichier
+    if ($handle->file_src_name_ext === 'jpg' || $handle->file_src_name_ext === 'jpeg' || $handle->file_src_name_ext === 'png' || $handle->file_src_name_ext === 'gif'){
+    $handle->process('./upload');
+      if ($handle->processed) {
+        include 'gmail.php';
+        $mail->addAttachment($handle->file_dst_pathname);
+        $handle->clean();
+      } else {
+        echo 'error : ' . $handle->error;
+      }
+}
+}
 ?>
