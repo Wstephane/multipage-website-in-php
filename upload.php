cd2dfc76d033c444e0382208325a473a007097e0
hakan poyraz <hakan.poyraz1994@gmail.com>
2018-08-08 10:35:05 +0200

    Injection sql finis normalement

--- upload.php
+++ upload.php
@@ -1,6 +1,16 @@
 <?php
 require 'vendor/autoload.php';
-  $handle = new upload($_FILES['image_field']);
+$gender = $_POST['gender'];
+$name =filter_var($_POST['name'],FILTER_SANITIZE_MAGIC_QUOTES);
+$first_name = filter_var($_POST['first_name'],FILTER_SANITIZE_MAGIC_QUOTES);
+$email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+$sujet = $_POST['sujet'];
+$msg = filter_var($_POST['msg'],FILTER_SANITIZE_MAGIC_QUOTES);
+$choix = $_POST['choix'];
+$handle = new upload($_FILES['image_field']);
+
+// 2. Validation
+if (true === filter_var($email, FILTER_VALIDATE_EMAIL)) {
   if ($handle->uploaded) {
     // Ajout d'une fin pour que le fichier soi unique
     $handle->file_name_body_add    = "_".round(microtime(true));
@@ -14,6 +24,10 @@ require 'vendor/autoload.php';
       } else {
         echo 'error : ' . $handle->error;
       }
-}
+  }
+  }
+
+} else {
+	echo "Cette adresse email nettoyée n'est pas valide. Désolé.";
 }
 ?>
