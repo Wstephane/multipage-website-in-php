16bd0ab58190beab0f067b09e062f213d0c77417
Wyns Stéphane <stewyns.pro@gmail.com>
2018-10-25 15:49:33 +0200

    Version finale

--- contact.php
+++ contact.php
@@ -32,67 +32,69 @@
   </header>
 
   <body>
-    <div class="container">
-        <h1>Contact</h1>
+    <section class="page-section cta">    
+        <div class="container">
+            <h1>Contact</h1>
 
-        <form class="form-horizontal" role="form" method="POST" action="upload2.php" enctype="multipart/form-data">
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="genre" id="mme" value="option1" checked>
-                <label class="form-check-label" for="mme">Mme</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="genre" id="melle" value="option2">
-                <label class="form-check-label" for="melle">Melle</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" name="genre" id="mr" value="option3">
-                <label class="form-check-label" for="mr">Mr.</label>
-            </div>
-            <div class="form-group">
-                <label for="nom">Nom</label>
-                <input type="text" class="form-control" id="nom" placeholder="nom">
-            </div>
-            <div class="form-group">
-                <label for="prenom">Prénom</label>
-                <input type="text" class="form-control" id="prenom" placeholder="Prénom">
-            </div>
-            <div class="form-group">
-                <label for="age">Age</label>
-                <input type="number" class="form-control" id="age">
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="email">
-            </div>
-            <div class="form-row align-items-center">
-                <div class="col-auto my-1">
-                    <label class="mr-sm-2" for="preference">Préférence</label>
-                    <select class="custom-select mr-sm-2" id="preference">
-                        <option selected>Devenir bénèvole</option>
-                        <option value="1">Demande d'information générale</option>
-                        <option value="2">Demande d'informatinon pour les dons</option>
-                    </select>
+            <form class="form-horizontal" role="form" method="POST" action="upload2.php" enctype="multipart/form-data">
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="genre" id="mme" value="option1" checked>
+                    <label class="form-check-label" for="mme">Mme</label>
                 </div>
-            </div>
-            <div class="form-group">
-                <label for="message">Votre message</label>
-                <textarea class="form-control" id="message" rows="5"></textarea>
-            </div>
-            <div class="form-group">
-                <label for="fichier">Document</label>
-                <input type="hidden" name="MAX_FILE_SIZE" value="100000">
-                <input type="file" class="form-control-file" id="fichier">
-            </div>
-            <div class="form-group">
-                <p>Format de réponse souhaité</p>
-                <input class="form-check-label" type="radio" name="type" id="formatFichier" value="option1" checked>
-                <label class="form-check-label" for="formatFichier">HTML</label>
-                <input class="form-check-label" type="radio" name="type" id="formatFichier2" value="option2">
-                <label class="form-check-label" for="formatFichier2">Texte</label>
-            </div>
-            <button type="submit" class="btn btn-default">Contactez-nous</button>
-        </form>
-    </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="genre" id="melle" value="option2">
+                    <label class="form-check-label" for="melle">Melle</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="genre" id="mr" value="option3">
+                    <label class="form-check-label" for="mr">Mr.</label>
+                </div>
+                <div class="form-group">
+                    <label for="nom">Nom</label>
+                    <input type="text" class="form-control" id="nom" placeholder="nom">
+                </div>
+                <div class="form-group">
+                    <label for="prenom">Prénom</label>
+                    <input type="text" class="form-control" id="prenom" placeholder="Prénom">
+                </div>
+                <div class="form-group">
+                    <label for="age">Age</label>
+                    <input type="number" class="form-control" id="age">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="email">
+                </div>
+                <div class="form-row align-items-center">
+                    <div class="col-auto my-1">
+                        <label class="mr-sm-2" for="preference">Préférence</label>
+                        <select class="custom-select mr-sm-2" id="preference">
+                            <option selected>Devenir bénèvole</option>
+                            <option value="1">Demande d'information générale</option>
+                            <option value="2">Demande d'informatinon pour les dons</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="message">Votre message</label>
+                    <textarea class="form-control" id="message" rows="5"></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="fichier">Document</label>
+                    <input type="hidden" name="MAX_FILE_SIZE" value="100000">
+                    <input type="file" class="form-control-file" id="fichier">
+                </div>
+                <div class="form-group">
+                    <p>Format de réponse souhaité</p>
+                    <input class="form-check-label" type="radio" name="type" id="formatFichier" value="option1" checked>
+                    <label class="form-check-label" for="formatFichier">HTML</label>
+                    <input class="form-check-label" type="radio" name="type" id="formatFichier2" value="option2">
+                    <label class="form-check-label" for="formatFichier2">Texte</label>
+                </div>
+                <button type="submit" class="btn btn-default">Contactez-nous</button>
+            </form>
+        </div>
+    </section>
     <?php
 
         // Sanitisation
--- donation_contact.php
+++ donation_contact.php
@@ -69,61 +69,63 @@
       </div>
     </section>
 
-    <div class="container" id="here2">
-      <div class="form-sec">
-        <h4 class="white">Formulaire de Contact</h4>
-        <form name="qryform" id="qryform" enctype="multipart/form-data" method="post" action="donation_contact.php" onsubmit="return(validate());" novalidate="novalidate">
-        <div class="form-group">
-          <input type="radio" name="gender" value="madame" >
-          <label class="white" for="Madame">Mme</label>
-          <input type="radio" name="gender" value="mademoiselle">
-          <label class="white" for="Mademoiselle">Melle</label>
-          <input type="radio" name="gender" value="monsieur">
-          <label class="white" for="Monsieur">Mr</label>
-        </div>
+    <section class="page-section cta">
+      <div class="container" id="here2">
+        <div class="form-sec">
+          <h4 class="white">Formulaire de Contact</h4>
+          <form name="qryform" id="qryform" enctype="multipart/form-data" method="post" action="donation_contact.php" onsubmit="return(validate());" novalidate="novalidate">
+          <div class="form-group">
+            <input type="radio" name="gender" value="madame" >
+            <label class="white" for="Madame">Mme</label>
+            <input type="radio" name="gender" value="mademoiselle">
+            <label class="white" for="Mademoiselle">Melle</label>
+            <input type="radio" name="gender" value="monsieur">
+            <label class="white" for="Monsieur">Mr</label>
+          </div>
 
-        <div class="form-group">
-          <label class="white">Nom:</label>
-          <input type="text" class="form-control" id="name" placeholder="Entrer votre Nom" name="name">
-        </div>
-        <div class="form-group">
-          <label class="white">Prénom:</label>
-          <input type="text" class="form-control" id="first_name" placeholder="Entrer votre Prénom" name="first_name">
-        </div>
-        <div class="form-group">
-          <label class="white">Email:</label>
-          <input type="email" class="form-control" id="email" placeholder="Enter votre Email" name="email">
-        </div>
+          <div class="form-group">
+            <label class="white">Nom:</label>
+            <input type="text" class="form-control" id="name" placeholder="Entrer votre Nom" name="name">
+          </div>
+          <div class="form-group">
+            <label class="white">Prénom:</label>
+            <input type="text" class="form-control" id="first_name" placeholder="Entrer votre Prénom" name="first_name">
+          </div>
+          <div class="form-group">
+            <label class="white">Email:</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter votre Email" name="email">
+          </div>
 
-        <div class="form-group">
-          <label class="white" for="sujet">Objet:</label>
-          <select class="form-control" id="sujet" name="sujet">
-            <option>Devenir bénèvole</option>
-            <option>Demande d'information génèrale</option>
-            <option>Demande d'information sur les donations</option>
-          </select>
+          <div class="form-group">
+            <label class="white" for="sujet">Objet:</label>
+            <select class="form-control" id="sujet" name="sujet">
+              <option>Devenir bénèvole</option>
+              <option>Demande d'information génèrale</option>
+              <option>Demande d'information sur les donations</option>
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label class="white">Votre message:</label>
+            <textarea name="msg" class="form-control" id="msg" placeholder="Entrer votre message"></textarea>
+          </div>
+
+          <div class="form-group">
+            <p class="white">Format de réponse souhaité</p>
+            <input type="radio" name="choix" value="html" >
+            <label class="white" for="html">HTML</label>
+            <input type="radio" name="choix" value="texte">
+            <label class="white" for="texte">Texte</label>
+          </div>
+
+          <p class="white">Prenez une image à uploader et appuyer sur "choisir une image" puis sur "ouvrir".</p>
+          <input type="file" size="32" name="image_field" value="">
+          <button type="submit" class="btn btn-default" name="salope">Envoyer</button>
+
+          </form>
         </div>
-
-        <div class="form-group">
-          <label class="white">Votre message:</label>
-          <textarea name="msg" class="form-control" id="msg" placeholder="Entrer votre message"></textarea>
-        </div>
-
-        <div class="form-group">
-          <p class="white">Format de réponse souhaité</p>
-          <input type="radio" name="choix" value="html" >
-          <label class="white" for="html">HTML</label>
-          <input type="radio" name="choix" value="texte">
-          <label class="white" for="texte">Texte</label>
-        </div>
-
-        <p class="white">Prenez une image à uploader et appuyer sur "choisir une image" puis sur "ouvrir".</p>
-        <input type="file" size="32" name="image_field" value="">
-        <button type="submit" class="btn btn-default" name="salope">Envoyer</button>
-
-        </form>
       </div>
-    </div>
+    </section>
 
     <footer>
       <?php include 'footer.php'; ?>
